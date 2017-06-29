f9dbe693b2b5e59e232d3510c89c03fd8e84b58e
Marijana Molnar <marijana@orangeiceberg.com>
2017-06-29 15:10:00 +0200

    addded method for seting self delivery point id

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,7 +1,21 @@
 Spree::CheckoutController.class_eval do
-  before_filter :load_self_delivery_points, :only => [:edit, :update]
+  before_action :load_self_delivery_points, :only => [:edit, :update]
+  before_action :set_self_delivery_point_id, only: [:update]
 
   private
+
+  def set_self_delivery_point_id
+    if params[:order].present? && params[:order][:shipments_attributes].present?
+      shipping_rate_id = params[:order][:shipments_attributes]["0"][:selected_shipping_rate_id].to_i
+      shipping_rate = Spree::ShippingRate.where(id: shipping_rate_id).first
+      shipping_method_id = shipping_rate.shipping_method.id
+
+      if shipping_method_id == 5
+        @order.self_delivery_point_id = "1"
+        @order.use_billing = false
+      end
+    end
+  end
 
   def load_self_delivery_points
     @self_delivery_points = Spree::SelfDeliveryPoint.ordered
@@ -10,7 +24,7 @@ Spree::CheckoutController.class_eval do
   # prevent spree_address_book gem to store user_id in address
   if self.method_defined?(:normalize_addresses)
     spree_address_book_normalize_addresses = instance_method(:normalize_addresses)
-  
+
     define_method :normalize_addresses do
       if @order.self_delivery?
         return
